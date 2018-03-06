c17590eb0a679c7c8643b3381600a642fb4275bf
yons <zzg19901210@163.com>
2018-03-06 22:09:11 +0800

    zhangzhigang
    增加视频推荐接口

--- pages/video_play/index.js
+++ pages/video_play/index.js
@@ -2,7 +2,9 @@
 
 const app = getApp();
 
-const setVodUrl = app.globalData.serverUrl + "vodUserAction/set.json";
+const setVodUrl = app.globalData.serverUrl + "/vodUserAction/set.json";
+
+const getRecommenVod = app.globalData.serverUrl + "/vodInfoAction/getRecommendVod.json";
 
 Page({
 
@@ -18,12 +20,8 @@ Page({
     errorimges:'/images/head.jpg',
     list: [{
       title: '鬼才许可贺岁片《狄仁杰之四大天王》',
-      ali_picurl: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=68521645b8fb43160e12722841cd2d46/ca1349540923dd540371d1e6db09b3de9c824839.jpg',
-      desc: '徐克 、 电影'
-    }, {
-      title: '鬼才许可贺岁片《狄仁杰之四大天王》',
-      ali_picurl: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=68521645b8fb43160e12722841cd2d46/ca1349540923dd540371d1e6db09b3de9c824839.jpg',
-      desc: '徐克 、 电影'
+      aliImageUrl: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=68521645b8fb43160e12722841cd2d46/ca1349540923dd540371d1e6db09b3de9c824839.jpg',
+      nickname: '徐克 、 电影'
     }
     ]
   },
@@ -42,7 +40,8 @@ Page({
       vodId:options.vodId,
       headPortrait: options.headPortrait
     })
-    // setVod(this);
+    setVod(this);
+    load(this);
     wx.setNavigationBarTitle({
       title: options.title,
       success: function (res) {
@@ -77,6 +76,22 @@ var setVod = function (that) {
 };
 //加载相关推荐信息
 var load=function(that){
-
+  wx.showLoading({
+    title: '请稍等...',
+  });
+  wx.request({
+    url: getRecommenVod,
+    header: {
+      'context-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    success: function (res) {
+      wx.hideLoading();
+      //设置数据
+      that.setData({
+        list: res.data.list
+      });
+    }
+  });
 }
 
